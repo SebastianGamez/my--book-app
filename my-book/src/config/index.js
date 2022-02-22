095b9140010fceb39c144412dd2339c2f28c089a
Sebastián Gámez <juan.gamez1001@gmail.com>
2022-02-22 13:30:38 -0500

    prepare the folder docs to deploy

--- my-book/src/config/index.js
+++ my-book/src/config/index.js
@@ -1,1 +1,1 @@
-export const server = process.env.REACT_APP_SERVER_URL || "http://localhost:4000";
+export const server = process.env.REACT_APP_SERVER_URL || "https://my-book-backend.herokuapp.com";
